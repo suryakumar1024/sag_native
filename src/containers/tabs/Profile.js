dd21acc818905ca701b7d521c7cfd04e8f8ccb9c
yogesh-k-64 <99863482+yogesh-k-64@users.noreply.github.com>
2022-03-18 12:31:06 +0530

    change to french by host click (#3)
    * change to french by host click
    
    * added calender view
    
    * Update .gitignore
    
    * Delete Gemfile.lock
    
    * gitignore work, removed unwanted files
    
    Co-authored-by: SURYA KUMAR K <suryakumark@sagcorps.com>

--- src/containers/tabs/Profile.js
+++ src/containers/tabs/Profile.js
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import {Agenda, Calendar, CalendarList} from 'react-native-calendars';
+import React from 'react';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={styles.calendar}>
+      <Calendar
+      firstDay={5}
+        onDayPress={day => {
+          console.log('selected day', day);
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  calendar: {
+    flex: 1,
+  },
+});
